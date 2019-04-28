c878cc5508f9cef4114176766ec8c9cbbb760ffe
ivanthewebber <42590900+ivanthewebber@users.noreply.github.com>
2019-04-27 17:41:45 -0700

    Update space.js
    Cleaned the code, added a couple comments, hopefully didn't break anything...

--- public/space.js
+++ public/space.js
@@ -10,39 +10,27 @@
 
 (function () {
     "use strict";
-    let steer = [false, false, false, false]; // tracks user interaction
-
-    // the values of the sprite representing our spaceship. If I do all of this right it will soon just be the first thing in the sprites list
-    // connect -> get sprite
-
-    // loop:
-    // draw
-    // update
-
-    // loop:
-    // send user's spaceship, mention laser fire (not the user's job to actually fire)
-    // get updated list of sprites
-
-    // else: loop: { get, start:drawExtrapolate } after:get { restart:drawExtrapolate & send }
-    // notice we won't need global variables
-
-    // todo: make sure I report the number of people playing on init
-    // TODO: make steering part of spaceship, or draw the user's spaceship special
+    // monitors the user's controls over the animation cycle
+    let steer = [false, false, false, false];
 
     /** Initialize Web Page */
     window.onload = function () {
         console.log("initializing page");
-        // initialize elements ******************
+
         let stars = initStarscape();
+        
+        // most of what we will do happens when the user continues to freeflight
         document.getElementById("takeoff").onclick = function(event) {
             console.log("taking off!!!");
                         
             // remove welcome window
-            document.getElementById("welcome").style.visibility = "hidden";
-
+            let welcome = document.getElementById("welcome");
+            welcome.style.visibility = "hidden";
+            welcome.innerHtml = ""; // prevents addition of scrollbar
+            
             // finish startup code
             initBattleground();
-            let sprites = initSprites(); // TODO 
+            let sprites = initSprites();
     
             // set handlers *************************
             document.onresize = initStarscape;
@@ -57,14 +45,13 @@
             let animation = setInterval(
                 function () {
                     drawAndUpdateStars(stars, sprites[0]);
-                    //transformRelativeToUser(sprites);
                     drawBattleground(sprites);
                     steerUserSpaceship(sprites[0]);
                     extrapolate(sprites);
                 }
-                , 30); // TODO: think about this number
+                , 30);
     
-            // update sprites (from server then transformed) over and over
+            // sync user's spaceship & other sprites with the server
             let sync = setInterval(
                 function () {
                     getSyncedSprites(sprites);
@@ -109,7 +96,7 @@
         let userSpaceship = {
             type: "spaceship",
 
-            x: Number(0), // TODO random? get from server
+            x: Number(0),
             y: Number(0),
             rotation: Number(0),
 
@@ -124,33 +111,21 @@
             laserGun: Boolean(false)
         }
 
-        // send the user's spaceship to the server
-        const fetchOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userSpaceship)
-        }
-        fetch("https://cyberspace-freeflight.herokuapp.com/", fetchOptions)
-            .then(function (response) {
-                console.log(response.text());
-            })
-
+        // until we get other sprites this will be all we have
         let sprites = [];
         sprites.push(userSpaceship);
+        getSyncedSprites(sprites);
+        
         return sprites;
     }
 
     /** draws all stars and concurrently updates their positions. Adds new stars as needed. */
     function drawAndUpdateStars(stars, spaceship) {
-        // console.log("drawing and updating stars");
         let space = document.getElementById("starscape");
         let context = space.getContext("2d");
 
-        // space is dark...
-        context.fillStyle = "darkblue";
+        // clear canvas
+        context.fillStyle = "darkblue"; // TODO make lower opacity to show motion blur
         context.fillRect(0, 0, space.width, space.height);
 
         // but we have stars
@@ -197,25 +172,24 @@
         }
     }
 
-    /** just updates that text */
+    /** updates the status text in the topleft. This is useful for testing if the user is connected to the server */
     function updateStatus(spaceship, numOthers) {
         let status = document.getElementById("status");
         status.innerText = numOthers + " active users\nx: " + spaceship.x + " y: " + spaceship.y;
     }
 
-    /** draws all sprites (tis the goal) */
+    /** draws all sprites relative to the user's spaceship */
     function drawBattleground(sprites) {
         let spaceship = sprites[0];
-
+        
         let battleground = document.getElementById("battleground");
         let context = battleground.getContext("2d");
+        
+        // determine the offsets needed
         let deltaX = (battleground.width / 2) - spaceship.x;
         let deltaY = (battleground.height / 2) - spaceship.y;
 
-        // console.log("drawing spaceship! rotation: " + rotation);
-        //let battleground = document.getElementById("battleground");
-
-        // clear
+        // clear battleground
         context.clearRect(0, 0, battleground.width, battleground.height);
 
         for (let sprite of sprites) { // draw each sprite
@@ -225,87 +199,29 @@
             context.translate(sprite.x + deltaX, sprite.y + deltaY);
             context.rotate(sprite.rotation);
 
-            switch (sprite.type) {
-                case "laser":
-                    console.log("a mother laser!!");
-                    context.strokeStyle = "red";
-                    context.lineWidth = 10;
+            // we use the same base images with alternative hue
+            context.filter = "hue-rotate(" + sprite.hue + "deg)";
+            
+            if (sprite.engine) { // afterburn underneath if engine is on
+                let afterburn = document.getElementById("afterburn");
+                context.drawImage(afterburn, -(spaceship.width * 6 / 8), -afterburn.height / 2);
+             }
 
-                    context.moveTo(sprite.x + deltaX, sprite.y + deltaY);
-                    context.lineTo(battleground.width, battleground.height);
-                    //context.lineTo(sprite.x + 5 * sprite.velocityX, sprite.y + 5 * sprite.velocityY);
-                    context.stroke();
+             if (sprite.laserGun) { // laserburn if laser is fired
+                 let laserburn = document.getElementById("laserburn");
+                 context.drawImage(laserburn, (spaceship.width / 3) + (laserburn.width), -laserburn.height / 2);
+             }
 
-                    // remove lasers out of bounds TODO this should be server mediated
-                    break;
-                case "spaceship":
-                    // we use the same base image with alternative saturation
-                    let spaceship = document.getElementById("spaceship");
-                    context.filter = "hue-rotate(" + sprite.hue + "deg)";
-                    if (sprite.engine) { // afterburn underneath if engine is on
-                        let afterburn = document.getElementById("afterburn");
-                        context.drawImage(afterburn, -(spaceship.width * 6 / 8), -afterburn.height / 2);
-                    }
-
-                    if (sprite.laserGun) { // laserburn if laser is fired
-                        let laserburn = document.getElementById("laserburn");
-                        context.drawImage(laserburn, (spaceship.width / 3) + (laserburn.width), -laserburn.height / 2);
-                    }
-
-                    // TODO: add side booster afterburns?
-
-                    // and then the actual image
-                    context.drawImage(spaceship, -spaceship.width / 2, -spaceship.height / 2);
-                    context.filter = "none";
-                    break;
-            }
+              // and always the spaceship itself
+              let spaceship = document.getElementById("spaceship");
+              context.drawImage(spaceship, -spaceship.width / 2, -spaceship.height / 2);
+            
             context.restore(); // go back to normal
         } // end draw each sprite
     }
 
-    function drawRotated(degrees) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        // save the unrotated context of the canvas so we can restore it later
-        // the alternative is to untranslate & unrotate after drawing
-        context.save();
-
-        // move to the center of the canvas
-        context.translate(canvas.width / 2, canvas.height / 2);
-
-        // rotate the canvas to the specified degrees
-        context.rotate(degrees * Math.PI / 180);
-
-        // draw the image
-        // since the context is rotated, the image will be rotated also
-        context.drawImage(image, -image.width / 2, -image.width / 2);
-
-        // we’re done with the rotating so restore the unrotated context
-        context.restore();
-    }
-
-    function fire() {
-        console.log("fire!!");
-        let spaceship = sprites[0];
-
-        // make a new sprite, I think I'll end up sending it to the server TODO?
-        let laser = {
-            type: "laser",
-            x: Number(0),
-            y: Number(0),
-            rotation: Number(spaceship.rotation),
-            velocityX: Number(spaceship.velocityX),
-            velocityY: Number(spaceship.velocityY),
-            rotationVelocity: Number(0)
-            //dueDate: Date.now() + 5000 // they had 1 second...
-        }
-
-        sprites.push(laser);
-    }
-
     /** listends to keys for movement and user interaction */
     function listenControls(e, spaceship) {
-
         e = e || window.event;
         // console.log(e.keyCode);
         if (e.keyCode == '38' || e.keyCode == '87') {
@@ -328,7 +244,7 @@
         }
     }
 
-    /** releases a key from it's pressed state */
+    /** releases a key from pressed state */
     function stopKeys(e, spaceship) {
         e = e || window.event;
         // console.log(e.keyCode);
@@ -352,7 +268,7 @@
         }
     }
 
-    /** check what the user is trying to do with the spaceship */
+    /** adjusts spaceship's vectors based on the users steering during an animation cycle  */
     function steerUserSpaceship(spaceship) {
 
         if (steer[0]) {
@@ -369,26 +285,13 @@
                 spaceship.velocityY = Math.max(-128, spaceship.velocityY + Math.sin(spaceship.rotation));
             }
         }
-        if (steer[1]) {
-            // maybe shoot? No brakes yo...
-            // Hell yeah! It's time for LASERS
-            // if (laserCharged) { // laser ready?
-            //     laserCharged = false;
-            //     fire();
-            //     setTimeout(function () {
-            //         laserCharged = true;
-            //     }, 800);
-            // }
-        }
         if (steer[2]) {
             // turn left
-            // console.log("left veer");
             spaceship.rotationVelocity = Math.max(-Math.PI / 32, spaceship.rotationVelocity - (Math.PI / 1024));
             spaceship.rotationVelocity = Math.min(Math.PI / 32, spaceship.rotationVelocity);
         }
         if (steer[3]) {
             // turn right
-            // console.log("go right!");
             spaceship.rotationVelocity = Math.max(-Math.PI / 32, spaceship.rotationVelocity + (Math.PI / 1024));
             spaceship.rotationVelocity = Math.min(Math.PI / 32, spaceship.rotationVelocity);
         }
@@ -412,23 +315,6 @@
         }
     }
 
-    /** Transforms all vectors such that the origin is the user's spaceship. Returns transformed sprites */
-    function transformRelativeToUser(sprites) {
-        // Adjusts all sprites's vectors to be relative to our canvas/user
-        let spaceship = sprites[0];
-
-        let battleground = document.getElementById("battleground");
-        let deltaX = (battleground.width / 2) - spaceship.x;
-        let deltaY = (battleground.height / 2) - spaceship.y;
-
-        for (let sprite of sprites) {
-            sprite.x += deltaX;
-            sprite.y += deltaY;
-        }
-
-        return sprites;
-    }
-
     /** replaces all sprites with updated sprites requested from the server */
     function getSyncedSprites(sprites) {
         let userSpaceship = sprites[0];
@@ -442,26 +328,26 @@
             credentials: 'include',
             body: JSON.stringify(userSpaceship)
         }
+        
         fetch("https://cyberspace-freeflight.herokuapp.com/", fetchOptions)
             .then(checkStatus)
             .then(function (response) {
-                //console.log(response.sprites);
-                // reset list
+                // sync (keeping the user spaceship because we have the authority on our location)
                 sprites.length = 1;
-                //sprites.push(response.userSpaceship);
-                //sprites = response.sprites;
                 for (let i = 1; i < response.sprites.length; ++i) {
                     sprites.push(response.sprites[i]);
                 }
 
-                updateStatus(userSpaceship, response.sprites.length); // abs position
-                return sprites;
+                // update our coordinates and the number of users
+                updateStatus(userSpaceship, response.sprites.length);
+                
+                return sprites; // we already updated them, but send them anyways
             })
     }
 
     function checkStatus(response) {
         if (response.status < 300) {
-            //console.log(response);
+            //console.log(response); // for error cases
             return response.json();
         } else {
             console.log("error: " + response);
