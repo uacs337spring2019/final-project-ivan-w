cb45e1829f1a9da0a6f84b23b561140ee3d72610
ivanthewebber <42590900+ivanthewebber@users.noreply.github.com>
2019-04-27 17:05:11 -0700

    Update space_service.js
    I cleaned up the code to reduce output to the log and make it cleaner

--- space_service.js
+++ space_service.js
@@ -2,8 +2,10 @@
 // 4/23/2019
 // Ivan is the origonal space pirate. Cross him and die!!!
 
+/* stores and distributes spaceships to enable users to see one another duing cyberspace freeflight! */
+
 // This code fights college debt
-console.log("let's start an epic space-war yo!");
+console.log("starting cyberspace freeflight service");
 
 // make sure everything is in order
 const express = require("express");
@@ -26,10 +28,9 @@ app.use(session({
     duration: 1 * 60 * 1000, // connect 1 minute
     activeDuration: 5 * 1000, // extend 5 seconds
     httpOnly: true,
-    // secure: true,
-    // ephemeral: true
 }));
 
+// prevent cors preflight errors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", 
@@ -37,14 +38,9 @@ app.use(function(req, res, next) {
 	next();
 });
 
-// helper code for running space
+/* Instead of setting up a database I just use a Map for matching cookies to users */
 let sprites = new Map();
-userNum = 0;
-
-function printSprites() {
-    console.log("we have " + sprites.length + " sprites");
-    console.log(sprites);
-}
+userNum = 0; // used to give unique ids
 
 function cleanSprites() {
     for (let [key, value] of sprites) {
@@ -54,69 +50,19 @@ function cleanSprites() {
     }
 }
 
-function updatePlayer(spaceship) {
-
-}
-
-// handle requests
-// app.get('/', function(req, res) {
-//     console.log("today we have " + sprites.size + " visitors");
-//     //res.header("Access-Control-Allow-Origin", "*");
-    
-//     if(req.cookies.spriteKey) {
-// 		res.send("You made the jump to GET " + req.cookies.spriteKey);
-// 	} else {
-// 		res.cookie('spriteKey', userNum++, {maxAge: 60*60, httpOnly: false});
-// 		console.log("cookie set at GET ");
-//     }
-
-//     let send = {};
-//     send.sprites = [];
-//     for (let sprite of sprites.values()) {
-//         send.sprites.push(sprite);
-//     }
-
-//     res.send(JSON.stringify(send));
-//     console.log(JSON.stringify(send));
-
-//     if (req.session) {
-//         console.log("GET: I knew that guy...");
-//     } else {
-//         console.log("Who are you?");
-//     }
-// })
-
 /** Add/Update Sprite: the user may send us sprites that we will store to our database. Each user is only allowed one spaceship, but we allow them to send lasers and explosions (accusations of collisions) */
 app.post('/', jsonParser, function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("today we have " + sprites.size + " visitors");
-    console.log("someone is sending us a spaceship!!!");
-
-    // if (req.body.spriteKey) {
-    //     // don't give someone their own spaceship
-    //     console.log("A return customer!");
-    //     console.log("it must be: " + req.session.spriteKey);
-    //     sprites.remove(req.body.spriteKey);
-    // } else {
-    //     // send this new person a greeting card
-        
-    // }
-    
-    
-    // if(req.cookies.spriteKey) {
-	// 	res.send("THE COOKIE MADE IT " + req.cookies.spriteKey);
-	// } else {
-	// 	res.cookie('spriteKey', userNum++, {maxAge: 60*60, httpOnly: false});
-	// 	console.log("cookie set at POST ");
-	// }
-
+	if ((sprites.size() % 100) == 1) {
+		console.log("we now have " + sprites.size() + " visitors");
+	}
+	
     if (!req.session.spriteKey) {
-        console.log("A new vessel has arrived!!!");
-        req.session.spriteKey = userNum++;
-        console.log("We'll store it under the name: " + req.session.spriteKey);
+	// new user, issue a spriteKey
+	req.session.spriteKey = userNum++;
     } else {
-        console.log("it's a MIRACLE ");
-        sprites.delete(req.session.spriteKey);
+	// user has returned
+	sprites.delete(req.session.spriteKey);
     }
 
     // prepare a list of all other sprites
@@ -132,7 +78,7 @@ app.post('/', jsonParser, function(req, res) {
         send.sprites.push(sprite);
     }
 
-    // add their sprite to our database
+    // add their updated sprite to our database
     userSpaceship.dueDate = Date.now() + 5000;
     sprites.set(req.session.spriteKey, userSpaceship);
     
@@ -140,34 +86,19 @@ app.post('/', jsonParser, function(req, res) {
     send.userSpaceship = userSpaceship;
     send.spriteKey = req.session.spriteKey;
     res.send(send);
-
-    if (req.body.type == "spaceship" ) {
-        // // store this to our database, replacing if already sent
-        // console.log(req.body);
-        // sprite.dueDate = Date.now() + 5000;
-        // sprites.set(req.session.spriteKey, sprite);
-
-        // console.log("We've updated the vessel under captain: " + req.session.spriteKey);
-    } else if (req.body.type == "laser") {
-        // we currently don't support lasers
-    } else if (req.body.type == "explosion") {
-        // we currently don't take explosions
-    }
 })
 
+// keep the server from getting glugged with old sprites
 setInterval(function(){
     console.log("I love cleaning!!!");
         // remove old sprites
         for (let [key, value] of sprites.entries()) {
-            if (!value.dueDate) {
-                console.log("Gotta get rid of this one...");
-                sprites.delete(key);
-            } else if(value.dueDate > Date.now()) {
-                console.log("You should've gone by now...");
+            if (!value.dueDate || value.dueDate > Date.now()) {
+                // get rid of expired or illigitimate sprites
                 sprites.delete(key);
             }
         }
-}, 5000)
+}, 5000) // once every 5 seconds
 
 // start listening
 app.listen(process.env.PORT);
